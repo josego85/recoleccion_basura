fbdab1a9131d15ff521b454fa983ba3b3a232f3e
josego85 <josego85@gmail.com>
2016-03-25 18:38:36 -0300

    Se muestra la informacion de los barrios

--- src/js/MapaOSM.js
+++ src/js/MapaOSM.js
@@ -19,23 +19,30 @@ function cargarMapa(p_coordenadas){
     $.getJSON("datos/barrios/Asuncion.geojson", function(data){
         v_layer_todos_barrios_asu = L.geoJson(data, {
 			style: estilo_barrios,
-			onEachFeature: popup
+			onEachFeature: onEachFeature
 		}).addTo(v_mapa);
 
 		// Info.
-		// var info = L.control();
-		// info.onAdd = function (v_mapa) {
-		//     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-		//     this.update();
-		//     return this._div;
-		// };
-		// // method that we will use to update the control based on feature properties passed
-		// info.update = function (props) {
-		//     this._div.innerHTML = '<h4>Barrio</h4>' +  (props ?
-		//         '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-		//         : 'Hover over a state');
-		// };
-		// info.addTo(v_mapa);
+		var info = L.control();
+		info.onAdd = function (v_mapa) {
+		    this._div = L.DomUtil.create('div', 'info');
+		    this.update();
+		    return this._div;
+		};
+		info.update = function (propiedades) {
+			var v_info_tmp;
+			if (propiedades && propiedades.nombre) {
+				var info_tmp = "<b>" + propiedades.nombre + "</b>";
+				if(propiedades.turno != null){
+					info_tmp += " (" + propiedades.turno + ")";
+				}
+				v_info_tmp = '<h4>Barrio</h4>' + info_tmp;
+			}else{
+			     v_info_tmp = '<h4>No es un barrio</h4>';
+			}
+		    this._div.innerHTML = v_info_tmp;
+		};
+		info.addTo(v_mapa);
 
 		// Leyenda.
 		var leyenda = L.control({position: 'bottomright'});
@@ -57,56 +64,96 @@ function cargarMapa(p_coordenadas){
 
 		// Mostrar ubicacion actual.
 		//mostrar(p_coordenadas);
-	});
 
-	// Muestra un pop por cada barrio.
-	function popup(feature, layer) {
-		if (feature.properties && feature.properties.nombre) {
-			var info;
-			if(feature.properties.turno != null){
-				info = feature.properties.nombre + " (" +
-			      feature.properties.turno + ")";
-			}else{
-				info = feature.properties.nombre;
+		////////////////////////////////////////////////////////////////////////
+		// Funciones internas.
+		////////////////////////////////////////////////////////////////////////
+
+		// Muestra un pop por cada barrio.
+		// function popup(feature, layer) {
+		// 	if (feature.properties && feature.properties.nombre) {
+		// 		var info;
+		// 		if(feature.properties.turno != null){
+		// 			info = feature.properties.nombre + " (" +
+		// 		      feature.properties.turno + ")";
+		// 		}else{
+		// 			info = feature.properties.nombre;
+		// 		}
+		// 		layer.bindPopup(info);
+		// 	}else if(feature){
+		// 		//console.log(feature);
+		// 		layer.bindPopup(feature);
+		// 	}
+		// }
+
+		//
+		// function mostrar(p_coordenadas){
+		// 	// Mostrar ubicacion actual.
+		// 	var layer = leafletPip.pointInLayer([p_coordenadas.longitud, p_coordenadas.latitud],
+		// 		v_layer_todos_barrios_asu, true);
+		// 		//console.log("v_layer_todos_barrios_asu: ", v_layer_todos_barrios_asu);
+		// 	if(layer.length) {
+		// 		popup(layer[0].feature.properties.nombre + " (" +
+		// 		  layer[0].feature.properties.turno + ")", v_layer_todos_barrios_asu);
+		// 	}else{
+		// 		 v_layer_todos_barrios_asu.bindPopup = '';
+		// 	}
+		// }
+
+		// Funcion que devuelve el estilo de un barrio.
+		function estilo_barrios(feature){
+			return {
+		        fillColor: getColor(feature.properties.turno),
+		        weight: 2,
+		        opacity: 1,
+		        color: 'white',
+		        dashArray: '3',
+		        fillOpacity: 0.7
+		    };
+		}
+
+		// Funcion que devuelve el color dependiendo
+		// del turno.
+		function getColor(turno) {
+		    return turno == "manhana" ? '#339848' :
+		           turno == "tarde"  ? '#F69522' :
+		           turno == "noche"  ? '#D9138e' :
+		               '#CEE49B';
+		}
+
+		// Funcion.
+		function highlightFeature(e) {
+			var layer = e.target;
+			layer.setStyle({
+				weight: 4,
+				color: '#5C1313',
+				dashArray: '',
+				fillOpacity: 0.7
+			});
+			if (!L.Browser.ie && !L.Browser.opera) {
+				layer.bringToFront();
 			}
-			layer.bindPopup(info);
-		}else if(feature){
-			//console.log(feature);
-			layer.bindPopup(feature);
+			info.update(layer.feature.properties);
 		}
-	}
 
-	//
-	function mostrar(p_coordenadas){
-		// Mostrar ubicacion actual.
-		var layer = leafletPip.pointInLayer([p_coordenadas.longitud, p_coordenadas.latitud],
-			v_layer_todos_barrios_asu, true);
-			//console.log("v_layer_todos_barrios_asu: ", v_layer_todos_barrios_asu);
-		if(layer.length) {
-			popup(layer[0].feature.properties.nombre + " (" +
-			  layer[0].feature.properties.turno + ")", v_layer_todos_barrios_asu);
-		}else{
-			 v_layer_todos_barrios_asu.bindPopup = '';
+		// Funcion.
+		function resetHighlight(e) {
+			v_layer_todos_barrios_asu.resetStyle(e.target);
+			info.update();
 		}
-	}
 
-	//
-	function estilo_barrios(feature){
-		return {
-	        fillColor: getColor(feature.properties.turno),
-	        weight: 2,
-	        opacity: 1,
-	        color: 'white',
-	        dashArray: '3',
-	        fillOpacity: 0.7
-	    };
-	}
+		// Funcion.
+		function zoomToFeature(e) {
+			v_mapa.fitBounds(e.target.getBounds());
+		}
 
-	//
-	function getColor(turno) {
-	    return turno == "manhana" ? '#339848' :
-	           turno == "tarde"  ? '#F69522' :
-	           turno == "noche"  ? '#D9138e' :
-	               '#CEE49B';
-	}
+		// Funcion.
+		function onEachFeature(feature, layer) {
+			layer.on({
+				mouseover: highlightFeature,
+				mouseout: resetHighlight,
+				click: zoomToFeature
+			});
+		}
+	});
 }
